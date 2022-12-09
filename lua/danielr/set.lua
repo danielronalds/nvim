04d0fc628c0ca13e200756515145eaedd73961f8
Daniel R <nzdanron@gmail.com>
2022-12-09 23:36:58 +1300

    Disabled swap files

--- lua/danielr/set.lua
+++ lua/danielr/set.lua
@@ -18,6 +18,9 @@ vim.opt.smartindent = true
 -- No wraping
 vim.opt.wrap = false
 
+-- No swap files
+ vim.cmd[[set noswapfile]]
+
 -- Hidden "https://medium.com/usevim/vim-101-set-hidden-f78800142855#:~:text=Typing%20%3Aset%20hidden%20will%20change,be%20hidden%20instead%20of%20closed"
 vim.opt.hidden = true
 
