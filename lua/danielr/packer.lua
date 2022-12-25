69b9ef7162b6ac2dfa6087aaa134428bc508ca4e
Daniel R <nzdanron@gmail.com>
2022-12-25 17:50:18 +1300

    Added Catpuccin colorscheme... don't like it

--- lua/danielr/packer.lua
+++ lua/danielr/packer.lua
@@ -9,6 +9,7 @@ return require('packer').startup(function(use)
 
     -- Themes
     use "gruvbox-community/gruvbox"
+    use { "catppuccin/nvim", as = "catppuccin" }
 
     -- Fuzzyfinder(Telescope)
     use {
