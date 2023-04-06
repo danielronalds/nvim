6a47470010dd85e65fd1ed18f74324db92a43a93
Daniel R <nzdanron@gmail.com>
2023-04-06 12:33:00 +1200

    Added Alpha
    Wanted a nice starting page, so added Alpha plugin

--- lua/danielr/packer.lua
+++ lua/danielr/packer.lua
@@ -44,6 +44,15 @@ return require('packer').startup(function(use)
         end
     }
 
+    -- alpha
+    use {
+        'goolord/alpha-nvim',
+        requires = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+            require'alpha'.setup(require'alpha.themes.startify'.config)
+        end
+    }
+
     -- WhichKey
     use {
         "folke/which-key.nvim",
