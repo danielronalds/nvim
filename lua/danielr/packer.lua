4299b19d1d2c5cc7eed6486842599feb7fb8bdea
Daniel R <nzdanron@gmail.com>
2023-12-07 23:51:12 +1300

    fix: Telescope erroring on run
    Applied fixes from the following thread for fixing Telescope.
    
    https://www.reddit.com/r/neovim/comments/1791a7x/all_of_a_sudden_i_cant_use_telescope/

--- lua/danielr/packer.lua
+++ lua/danielr/packer.lua
@@ -15,7 +15,7 @@ return require('packer').startup(function(use)
 
     -- Fuzzyfinder(Telescope)
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use { 'stevearc/dressing.nvim' }
