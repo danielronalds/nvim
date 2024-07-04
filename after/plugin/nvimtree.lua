bcff385cd85acbfc95bb5fc5fdb7917a31cebd59
Daniel R <nzdanron@gmail.com>
2024-07-05 10:34:40 +1200

    feat: add config for nvim tree
    - Opens on right
    - Expands folders to show current file

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,11 +1,16 @@
 vim.cmd[[hi NvimTreeNormal guibg=NONE ctermbg=NONE]]
 
 require("nvim-tree").setup({
+  update_focused_file = {
+      enable = true,
+      update_cwd = true,
+  },
   sort = {
     sorter = "case_sensitive",
   },
   view = {
     width = 30,
+    side = 'right',
   },
   renderer = {
     group_empty = true,
