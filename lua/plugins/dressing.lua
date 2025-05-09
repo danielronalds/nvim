09cf467404b2c7a61fee66e3dd3f7ccca5f5563c
Daniel R <nzdanron@gmail.com>
2025-05-10 11:47:02 +1200

    feat: add snacks

--- lua/plugins/dressing.lua
+++
@@ -1,7 +0,0 @@
-return {
-    -- Expecting this to break... it has been archived
-    "stevearc/dressing.nvim",
-    config = function ()
-        require("dressing").setup()
-    end
-}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,8 @@ return {
     lazy = true,
     opts = {},
     keys = {
-        { "<leader>f",  function() require("telescope.builtin").find_files() end, desc = "Find Files" },
-        { "<leader>b",  function() require("telescope.builtin").buffers() end,    desc = "Search Buffers" },
-        { "<leader>lg", function() require("telescope.builtin").live_grep() end,  desc = "Open LiveGrep" },
+        --{ "<leader>f",  function() require("telescope.builtin").find_files() end, desc = "Find Files" },
+        --{ "<leader>b",  function() require("telescope.builtin").buffers() end,    desc = "Search Buffers" },
+        --{ "<leader>lg", function() require("telescope.builtin").live_grep() end,  desc = "Open LiveGrep" },
     }
 }
