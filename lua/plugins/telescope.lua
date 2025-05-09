4bba9e6de9fbaf7e4064f1d406a04301cbaa6030
Daniel R <nzdanron@gmail.com>
2025-05-10 00:39:57 +1200

    feat: lazy load telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,12 @@
 return {
-    "nvim-telescope/telescope.nvim", tag = "0.1.8",
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = false,
-    config = function()
-        require("telescope").setup()
-
-        local builtin = require("telescope.builtin")
-
-        vim.keymap.set("n", "<leader>f", function() builtin.find_files() end)
-        vim.keymap.set("n", "<leader>b", function() builtin.buffers() end)
-        vim.keymap.set("n", "<leader>lg", function() builtin.live_grep() end)
-    end
+    lazy = true,
+    opts = {},
+    keys = {
+        { "<leader>f",  function() require("telescope.builtin").find_files() end, desc = "Find Files" },
+        { "<leader>b",  function() require("telescope.builtin").buffers() end,    desc = "Search Buffers" },
+        { "<leader>lg", function() require("telescope.builtin").live_grep() end,  desc = "Open LiveGrep" },
+    }
 }
