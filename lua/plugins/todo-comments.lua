cca19b97a6c69f9a1f48cd8a8455c93a430bd665
Daniel R <nzdanron@gmail.com>
2025-05-10 13:28:31 +1200

    fix: todo-comments highlighting not working by default
    Not lazy loading this plugin

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
     opts = {},
     keys = {
         { "<leader>pp", function() Snacks.picker.todo_comments() end, desc = "Open Todo" },
