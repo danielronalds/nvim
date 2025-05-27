da6e36fdcb6e89d8cab12b670e5423f9d4361e77
Daniel R <nzdanron@gmail.com>
2025-05-28 11:10:47 +1200

    refactor: split treesitter config call into opts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            auto_install = true,
+    opts = {
+        auto_install = true,
 
-            highlight = {
-                enable = true,
-            }
-        })
+        highlight = {
+            enable = true,
+        }
+    },
+    config = function(_, opts)
+        require('nvim-treesitter.configs').setup(opts)
     end
 }
