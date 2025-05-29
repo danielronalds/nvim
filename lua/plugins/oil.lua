90ace9bf3e95e54528994efb6cd548af643d6438
Daniel R <nzdanron@gmail.com>
2025-05-29 19:46:52 +1200

    feat: add vim vinegar keybinding to oil
    Feels more ergonomic?

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,6 +3,7 @@ return {
     lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons", opts = {} },
     keys = {
+        { "-", function() require("oil").open() end, desc = "Open Parent Directory" },
         { "<leader>pv", function() require("oil").open() end, desc = "Open Oil" },
         { "<leader>sv", "<cmd>lua vim.cmd('vsplit | wincmd l'); require('oil').open()<cr>", desc = "Open Oil VSplit" },
         { "<leader>sh", "<cmd>lua vim.cmd('split | wincmd j'); require('oil').open()<cr>", desc = "Open Oil HSplit" },
