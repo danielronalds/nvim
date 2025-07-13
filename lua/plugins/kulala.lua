3aa222a9ec0f97c403966d8d8dc31ff890d38c01
Daniel Ronalds <daniel.ronalds@signinsolutions.com>
2025-07-14 08:56:47 +1200

    chore: plugin update

--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -2,11 +2,11 @@ return {
     "mistweaverco/kulala.nvim",
     lazy = true,
     ft = { "http", "rest" },
-    opts = {
-        global_keymaps = false,
-        global_keymaps_prefix = "<leader>R",
-        kulala_keymaps_prefix = "",
-    },
+    --opts = {
+    --    global_keymaps = false,
+    --    global_keymaps_prefix = "<leader>R",
+    --    kulala_keymaps_prefix = "",
+    --},
     keys = {
         { "<leader>Rs", desc = "Send request" },
         { "<leader>Ra", desc = "Send all requests" },
