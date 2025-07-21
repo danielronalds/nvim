9be59ec60bdad35c494c7a9a5216f8a679afb021
Daniel Ronalds <daniel.ronalds@signinsolutions.com>
2025-07-21 16:10:18 +1200

    feat: copilot only enabled on typescript files
    I only want it enabled on typescript files (i.e. during work)

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,15 +1,20 @@
 return {
-  { "github/copilot.vim" },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    {
+        "github/copilot.vim",
+        lazy = true,
+        ft = { "typescript" }
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-        agent = "copilot",
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "github/copilot.vim" },                 -- or zbirenbaum/copilot.lua
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        ft = { "typescript" },
+        build = "make tiktoken", -- Only on MacOS or Linux
+        opts = {
+            agent = "copilot",
+        },
+        -- See Commands section for default commands if you want to lazy load on them
     },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
 }
