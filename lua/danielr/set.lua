bacfc5e12a1ea9614616ae925bc40f9c09067947
Daniel Ronalds <nzdanron@gmail.com>
2025-08-08 08:12:24 +0000

    feat: set autoread true

--- lua/danielr/set.lua
+++ lua/danielr/set.lua
@@ -17,7 +17,10 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 -- No swap files
- vim.cmd[[set noswapfile]]
+vim.cmd[[set noswapfile]]
+
+-- Refresh buffer when changed externally
+vim.opt.autoread = true
 
 -- Hidden "https://medium.com/usevim/vim-101-set-hidden-f78800142855#:~:text=Typing%20%3Aset%20hidden%20will%20change,be%20hidden%20instead%20of%20closed"
 vim.opt.hidden = true
