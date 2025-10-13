eb5521569b883213e23d10aacc18713f728ee83f
Daniel Ronalds <nzdanron@gmail.com>
2025-10-14 09:42:53 +1300

    feat: add opencode plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,18 +3,5 @@ return {
         "github/copilot.vim",
         lazy = true,
         ft = { "typescript" }
-    },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        dependencies = {
-            { "github/copilot.vim" },                 -- or zbirenbaum/copilot.lua
-            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-        },
-        ft = { "typescript" },
-        build = "make tiktoken", -- Only on MacOS or Linux
-        opts = {
-            agent = "copilot",
-        },
-        -- See Commands section for default commands if you want to lazy load on them
-    },
+    }
 }
---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,21 @@
+return {
+    "NickvanDyke/opencode.nvim",
+    lazy = true,
+    config = function()
+        vim.g.opencode_opts = {
+            auto_reload = true,
+        }
+    end,
+    keys = {
+        { "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end,  mode = { "n", "x" }, desc = "Ask about this" },
+        { "<leader>o+", function() require("opencode").prompt("@this") end,                    mode = { "n", "x" }, desc = "Add this" },
+        { "<leader>os", function() require("opencode").select() end,                           mode = { "n", "x" }, desc = "Select prompt" },
+        { "<leader>ot", function() require("opencode").toggle() end,                           desc = "Toggle embedded" },
+        { "<leader>oc", function() require("opencode").command() end,                          desc = "Select command" },
+        { "<leader>on", function() require("opencode").command("session_new") end,             desc = "New session" },
+        { "<leader>oi", function() require("opencode").command("session_interrupt") end,       desc = "Interrupt session" },
+        { "<leader>oA", function() require("opencode").command("agent_cycle") end,             desc = "Cycle selected agent" },
+        { "<S-C-u>",    function() require("opencode").command("messages_half_page_up") end,   desc = "Messages half page up" },
+        { "<S-C-d>",    function() require("opencode").command("messages_half_page_down") end, desc = "Messages half page down" },
+    }
+}
