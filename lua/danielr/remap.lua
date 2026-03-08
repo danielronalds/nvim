af15f77a6364e27a267eb3eba6ce13e6733c3a1a
Daniel Ronalds <nzdanron@gmail.com>
2026-03-09 12:56:13 +1300

    chore: remove redundant yf keymap in favour of code-bridge

--- lua/danielr/remap.lua
+++ lua/danielr/remap.lua
@@ -7,5 +7,3 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Centers the screen after Ctrl + D and Ctrl + U
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-
-vim.keymap.set("n", "<leader>yf", ":let @+ = expand('%')<cr>", { desc = "Copy current buffer filepath" })
